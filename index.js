e5388960e8abd63c6a427c8b1a4b7a36f432a932
TonyInfinity <quachhengtony@gmail.com>
2021-01-28 21:58:02 +0700

    add process.env.port directly

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
-const PORT = process.env.port || 8000;
 
 const feedData = [];
 const resData = [];
@@ -63,4 +62,4 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT);
+app.listen(process.env.port || 8000);
